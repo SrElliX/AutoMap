adcfcc35988d51a6cdbef94cc39b499c1fe216e7
Eliel <87979824+SrElliX@users.noreply.github.com>
2023-12-26 11:36:13 -0300

    AutoMap v1
    Primeira versão do teste.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # AutoMap
- Meu primeiro grande projeto python para automatizar testes de penetração.
+
+# Projeto Nmap Automatizado
+
+Este projeto foi desenvolvido como parte do processo de aprendizado na área de programação e segurança cibernética. É um script Python interativo que utiliza a ferramenta Nmap para realizar varreduras em redes.
+
+## Descrição
+
+O **Nmap Automatizado** visa simplificar o processo de utilização do Nmap, uma poderosa ferramenta de código aberto para descoberta de dispositivos e serviços em uma rede. O script permite ao usuário realizar varreduras personalizadas, filtrar e visualizar informações específicas, proporcionando uma experiência interativa e educativa.
+
+## Funcionalidades
+
+- Varredura rápida de uma rede ou de um único IP.
+- Opções personalizadas de varredura, como serviços específicos e faixas de portas.
+- Filtragem da saída para exibição de informações relevantes.
+- Possibilidade de verificar portas específicas.
+- Opção de sair do programa a qualquer momento.
+
+## Utilização
+
+1. Clone o repositório para o seu ambiente local.
+   ```bash
+   git clone https://github.com/SrElliX/AutoMap.git
+   cd nmap-automatizado
+2. Execute o script Python.
+    python nmap_automatizado.py
+3. Siga as instruções interativas para inserir o IP ou a faixa de IP a ser varrida, bem como as opções de varredura desejadas.
+
+4. Explore as opções de filtragem para visualizar informações específicas na saída do Nmap.
+
+# Aviso
+
+Este projeto está em fase de desenvolvimento e teste, e é destinado principalmente para estudos pessoais na área de programação e segurança cibernética. Recomenda-se o uso em ambientes de teste e laboratórios virtuais. O autor é um iniciante nesse campo, e feedbacks são bem-vindos para aprimorar o projeto.
+
+**Observação:** Certifique-se de ter o Nmap instalado em seu sistema antes de usar este script.
+
+Espero que este projeto contribua para seus estudos e práticas na área de segurança cibernética. Sinta-se à vontade para contribuir, relatar problemas ou fornecer sugestões para aprimoramentos.
---
+++ automap.py
@@ -0,0 +1,79 @@
+import os
+import subprocess
+
+os.system('clear')
+
+# Códigos ANSI para cores
+RED = '\033[91m'
+RESET = '\033[0m'
+
+texts = [
+    '   _          _         __  __',
+    r'  /_\   _  _ | |_  ___ |  \/  | __ _  _ __',
+    r" / _ \ | || ||  _|/ _ \| |\/| |/ _` || '_ |",
+    r'/_/ \_\ \_,_| \__|\___/|_|  |_|\__,_|| .__|',
+    '                                     |_|'
+]
+
+for text in texts:
+    print(f'{RED}{text}{RESET}')
+
+
+def run_nmap(ip, scan_options):
+    try:
+        # Construa o comando nmap com base nas opções fornecidas
+        command = ['nmap'] + scan_options + [ip]
+
+        # Execute o comando nmap
+        result = subprocess.run(command, capture_output=True, text=True)
+
+        # Imprima a saída do nmap
+        print(result.stdout)
+
+        # Solicita ao usuário opções para filtrar a saída
+        print("\nOpções de filtragem:")
+        print("[1] Filtrar por serviços e portas")
+        print("[2] Filtrar por uma porta específica")
+        print("[S] Sair")
+        choice = input("Escolha uma opção: ")
+
+        if choice == '1':
+            # Filtrar por serviços e portas
+            services_ports = input("Digite os serviços/portas separados por espaço: ")
+            for line in result.stdout.splitlines():
+                if any(service_port in line for service_port in services_ports.split()):
+                    print(line)
+
+        elif choice == '2':
+            # Filtrar por uma porta específica
+            specific_port = input("Digite a porta específica: ")
+            for line in result.stdout.splitlines():
+                if f"{specific_port}/" in line:
+                    print(line)
+
+        elif choice.upper() == 'S':
+            # Sair do programa
+            print("Saindo do programa.")
+            exit()
+
+        else:
+            print("Opção inválida.")
+
+    except Exception as e:
+        print(f"\nErro ao executar o nmap: {e}")
+
+def main():
+    # Solicita ao usuário o IP a ser varrido
+    ip = input("\nDigite o IP ou a faixa de IP a ser varrida: ")
+
+    while True:
+        # Solicita ao usuário as opções de varredura
+        print("\nEscolha as opções de varredura (separadas por espaço):")
+        print("Exemplo: -sS -p 1-1000 -A")
+        scan_options = input("Opções: ").split()
+
+        # Executa o nmap com base nas escolhas do usuário
+        run_nmap(ip, scan_options)
+
+if __name__ == "__main__":
+    main()
